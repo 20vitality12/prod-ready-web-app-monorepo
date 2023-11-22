68c2de9c95630a48b8255cf1b69cc00a09a3e072
Vitalii Moroz <20vitality12@gmail.com>
2023-11-22 12:26:54 +0200

    fix: remove commented lines

--- apps/iac/src/environment/index.ts
+++ apps/iac/src/environment/index.ts
@@ -1,35 +1,25 @@
 import { stageType as stageType } from '@monolith/contracts'
 
-// const STAGE = process.env['STAGE'] as stageType
-// if (!STAGE) throw new Error('STAGE is undefined')
-//
-// const DOMAIN = process.env['DOMAIN']
-// if (!DOMAIN) throw new Error('DOMAIN is undefined')
-//
-// const SERVER_SUBDOMAIN = process.env['SERVER_SUBDOMAIN']
-// if (!SERVER_SUBDOMAIN) throw new Error('SERVER_SUBDOMAIN is undefined')
-//
-// const CLIENT_SUBDOMAIN = process.env['CLIENT_SUBDOMAIN']
-// if (!CLIENT_SUBDOMAIN) throw new Error('CLIENT_SUBDOMAIN is undefined')
-//
-// const HOSTED_ZONE_ID = process.env['HOSTED_ZONE_ID']
-// if (!HOSTED_ZONE_ID) throw new Error('HOSTED_ZONE_ID is undefined')
-//
-// const COMPANY_NAME = process.env['COMPANY_NAME']
-// if (!COMPANY_NAME) throw new Error('COMPANY_NAME is undefined')
+const STAGE = process.env['STAGE'] as stageType
+if (!STAGE) throw new Error('STAGE is undefined')
 
-// const IS_PRODUCTION = process.env['STAGE'] as stageType === 'production'
-// const BUILD_ASSETS = process.env['BUILD_ASSETS'] === 'true'
+const DOMAIN = process.env['DOMAIN']
+if (!DOMAIN) throw new Error('DOMAIN is undefined')
 
+const SERVER_SUBDOMAIN = process.env['SERVER_SUBDOMAIN']
+if (!SERVER_SUBDOMAIN) throw new Error('SERVER_SUBDOMAIN is undefined')
 
-const STAGE = 'develop' as stageType
-const DOMAIN='monolith-it.click'
-const SERVER_SUBDOMAIN='server'
-const CLIENT_SUBDOMAIN='client'
-const HOSTED_ZONE_ID='Z04759982J8OLX7XWH4GB'
-const COMPANY_NAME='monolith-it'
-const BUILD_ASSETS = true
-const IS_PRODUCTION = false
+const CLIENT_SUBDOMAIN = process.env['CLIENT_SUBDOMAIN']
+if (!CLIENT_SUBDOMAIN) throw new Error('CLIENT_SUBDOMAIN is undefined')
+
+const HOSTED_ZONE_ID = process.env['HOSTED_ZONE_ID']
+if (!HOSTED_ZONE_ID) throw new Error('HOSTED_ZONE_ID is undefined')
+
+const COMPANY_NAME = process.env['COMPANY_NAME']
+if (!COMPANY_NAME) throw new Error('COMPANY_NAME is undefined')
+
+const IS_PRODUCTION = process.env['STAGE'] as stageType === 'production'
+const BUILD_ASSETS = process.env['BUILD_ASSETS'] === 'true'
 
 const getSubdomainByStage = (domain: string): string => STAGE === 'production' ? domain : `${domain}.${STAGE}`
 export const environment = {
